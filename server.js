bdabbe5a0d29226668271aed17905aa26d5b2e8d
zaephyr <uros.mahne@gmail.com>
2020-11-15 14:29:51 +0100

    rdy for first deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ process.on('uncaughtException', (err) => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.MONGODB_URI || process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose
     .connect(DB, {
